f267de84f62cd53b796104cc337203511eba6afa
KempiG <84987806+KempiG@users.noreply.github.com>
2023-11-30 23:05:23 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -182,7 +182,7 @@ def main():
                 chart_data['Date'] = pd.to_datetime(chart_data['Date'])
                 chart_data['Month'] = chart_data['Date'].apply(lambda x: x.strftime('%B-%Y'))
                 st.dataframe(chart_data)
-                st.bar_chart(chart_data, x='Month',y='Number of installations',group=False)
+                st.bar_chart(chart_data, x='Month',y='Number of installations')
 
                 st.scatter_chart(chart_data, x='Date',y='Number of installations')
             
