af3b280310c7eb9d682559a77d88b98f9e0f3e6c
KempiG <84987806+KempiG@users.noreply.github.com>
2023-11-30 15:53:04 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -104,7 +104,7 @@ def main():
                 st.write(f'{file_} file uploaded')
                 #df = pd.read_excel(file_,sheet_name=1,header=1,skiprows=[2,3,4,5,6])
                 #st.dataframe(df)
-                data = pd.ExcelFile(file_)
+                data = pd.ExcelFile(file_,header=1,skiprows=[2,3,4,5,6])
                 output=pd.DataFrame()
                 names = data.sheet_names
                 for name in names:
