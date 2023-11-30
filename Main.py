456963ef56f1643e791ac454425483a587ba101b
KempiG <84987806+KempiG@users.noreply.github.com>
2023-11-30 16:20:02 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -135,7 +135,7 @@ def main():
                     #frame = CDC_funcs.convert(list_, radio1, radio2, new_name) 
                     fig, ax = plt.subplots(figsize=[18,3], facecolor='white')
                     ax.plot(df.iloc[:,7],df.index)
-                    ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
+                    ax.xaxis.set_major_formatter(md.DateFormatter('%H:%M'))
                     #fig.suptitle(f'{date} - {base_unit}', fontsize=20)
                     ax.grid(linestyle="--")
                     fig.autofmt_xdate()    
