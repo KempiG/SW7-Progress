e6bc33864a30d66b011f575261bfe3659618359f
KempiG <84987806+KempiG@users.noreply.github.com>
2023-11-30 11:49:50 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -9,6 +9,55 @@ import streamlit as st
 from PIL import Image
 import datetime
 
+
+def download_link_csv(object_to_download, download_filename, download_link_text):
+    """
+    Generates a link to download the given object_to_download.
+
+    object_to_download (str, pd.DataFrame):  The object to be downloaded.
+    download_filename (str): filename and extension of file. e.g. mydata.csv
+    download_link_text (str): Text to display for download link.
+
+    Examples:
+    download_link(YOUR_DF, 'YOUR_DF.csv', 'Click here to download data!')
+    download_link(YOUR_STRING, 'YOUR_STRING.txt', 'Click here to download your text!')
+
+    """
+    if isinstance(object_to_download, pd.DataFrame):
+        object_to_download = object_to_download.to_csv(index=True)
+    
+    b64 = base64.b64encode(object_to_download.encode()).decode()
+
+    return f'<a href="data:file/txt;base64,{b64}" download="{download_filename}">{download_link_text}</a>'
+
+
+def download_link_excel(object_to_download, download_filename, download_link_text):
+    """
+    Generates a link to download the given object_to_download.
+
+    object_to_download (str, pd.DataFrame):  The object to be downloaded.
+    download_filename (str): filename and extension of file. e.g. mydata.csv
+    download_link_text (str): Text to display for download link.
+
+    Examples:
+    download_link(YOUR_DF, 'YOUR_DF.csv', 'Click here to download data!')
+    download_link(YOUR_STRING, 'YOUR_STRING.txt', 'Click here to download your text!')
+
+    """
+    if isinstance(object_to_download, pd.DataFrame):
+        towrite = io.BytesIO()
+        object_to_download = object_to_download.to_excel(towrite, index=True)
+        towrite.seek(0)
+    
+    b64 = base64.b64encode(towrite.read()).decode()
+
+    return f'<a href="data:file/txt;base64,{b64}" download="{download_filename}">{download_link_text}</a>'
+
+
+
+
+
+
 st.set_page_config(layout="wide", 
                    page_icon='wise_men_crown.ico',
                    page_title='Seaway7 progress overview',
@@ -18,7 +67,7 @@ st.set_page_config(layout="wide",
 
 
 header1, header2 = st.columns([1,5])
-image1 = Image.open('Cofra_logo.PNG')
+image1 = Image.open('wise_men_crown.ico')
 header1.image(image1, width=150)
 
 horizontal_line = '''
@@ -31,7 +80,7 @@ def main():
     
     technique = st.sidebar.radio("""Choose visualization""", ['General project overview', 'Details'])
     
-    #####CDC#####
+    #####General info#####
     if technique == 'General project overview':
         header2.title("""General project data import""")
 
@@ -70,18 +119,18 @@ def main():
                     frame = CDC_funcs.convert(list_, radio1, radio2, new_name)    
                 
                     if radio2 == 'CSV file':
-                        tmp_download_link = CDC_funcs.download_link_csv(frame, 
+                        tmp_download_link = download_link_csv(frame, 
                                                                         f'{new_name}.csv', 
                                                                         'Click here to download CSV file!')
                     elif radio2 == 'Excel file':
-                        tmp_download_link = CDC_funcs.download_link_excel(frame, 
+                        tmp_download_link = download_link_excel(frame, 
                                                                           f'{new_name}.xlsx', 
                                                                           'Click here to download excel file!')
                     st.markdown(tmp_download_link, unsafe_allow_html=True)                    
                     
                 st.markdown(horizontal_line)
                 
-                CDC_funcs.show_preview(frame)
+                #CDC_funcs.show_preview(frame)
                 st.success('Done!')
 
   #####PVD#####
@@ -91,7 +140,7 @@ def main():
         uploads = st.sidebar.file_uploader('Upload log files', 
                                            accept_multiple_files=True, 
                                            type='ext',
-                                           help="""Select one or multiple EXT files with PVD data""")                
+                                           help="""Select one or multiple Excel files with project data""")                
         radio1 = st.sidebar.radio('Save as?', ['Excel file','CSV file'])
         #radio2 = st.sidebar.radio('Show platform thickness map?', ['No', 'Yes'])
         
@@ -141,86 +190,86 @@ def main():
                         
             st.markdown(horizontal_line)
             
-            if st.session_state.bool:
-                with st.spinner(text='In progress...'):
-                    frame, time_text = PVD_funcs.convert(list_, headerlist, wp_select, fixed_nr)
+            #if st.session_state.bool:
+            #    with st.spinner(text='In progress...'):
+            #        frame, time_text = PVD_funcs.convert(list_, headerlist, wp_select, fixed_nr)
                     
                     ## Output on screen ##    
-                    col1, col2, col3 = st.columns(3)
-                    col1.metric('Number of drains [-]', len(frame))
-                    col2.metric('Linear meter [m]', round(frame['Max. depth [m]'].sum(),2))
-                    col3.metric('Average installation depth [m]', round(frame['Max. depth [m]'].mean(),2))
+             #       col1, col2, col3 = st.columns(3)
+             #       col1.metric('Number of drains [-]', len(frame))
+             #       col2.metric('Linear meter [m]', round(frame['Max. depth [m]'].sum(),2))
+             #       col3.metric('Average installation depth [m]', round(frame['Max. depth [m]'].mean(),2))
                     
                     ## Download link ##
-                    if radio1 == 'CSV file':
-                        tmp_download_link = CDC_funcs.download_link_csv(frame, 
-                                                                        'PVD_data_processed.csv', 
-                                                                        'Click here to download CSV file!')
-                    elif radio1 == 'Excel file':
-                        tmp_download_link = CDC_funcs.download_link_excel(frame, 
-                                                                          'PVD_data_processed.xlsx', 
-                                                                          'Click here to download excel file!')
-                    st.markdown(tmp_download_link, unsafe_allow_html=True)                  
+             #       if radio1 == 'CSV file':
+             #           tmp_download_link = CDC_funcs.download_link_csv(frame, 
+             #                                                           'PVD_data_processed.csv', 
+             #                                                           'Click here to download CSV file!')
+             #       elif radio1 == 'Excel file':
+             #           tmp_download_link = CDC_funcs.download_link_excel(frame, 
+             #                                                             'PVD_data_processed.xlsx', 
+             #                                                             'Click here to download excel file!')
+             #      st.markdown(tmp_download_link, unsafe_allow_html=True)                  
                 
                 
-                st.markdown(horizontal_line)
+             #   st.markdown(horizontal_line)
                 
                 
-                st.write('**Preview:**')
-                PVD_funcs.show_preview(frame)
+             #  st.write('**Preview:**')
+             # PVD_funcs.show_preview(frame)
 
 
-                st.markdown(horizontal_line)
+             #    st.markdown(horizontal_line)
                 
                 
-                st.write('**Productivity:**')
-                col1, col2 = st.columns(2)
+             #   st.write('**Productivity:**')
+             #   col1, col2 = st.columns(2)
                 
-                base_units = []
-                dates = []
-                for file in list_:
-                    base_unit = file['Base unit [-]'].iloc[0]
-                    base_units.append(base_unit)
-                    date = file['date [YYYYMMDD]'].iloc[0]
-                    dates.append(date)
+             #   base_units = []
+             #   dates = []
+             #   for file in list_:
+             #       base_unit = file['Base unit [-]'].iloc[0]
+             #       base_units.append(base_unit)
+             #       date = file['date [YYYYMMDD]'].iloc[0]
+             #       dates.append(date)
                                 
-                dates_str = [str(x) for x in dates]
-                dates_str = [x[6:]+'-'+x[4:6]+'-'+x[:4] for x in dates_str]
-                date_base_units = []
-                date_base_units = [dates_str[i] + ' / ' + base_units[i] for i in range(len(dates_str))]
-                date_base_units.sort()
-                date_base_unit = st.selectbox('Select date + base unit', date_base_units)
+             #   dates_str = [str(x) for x in dates]
+             #   dates_str = [x[6:]+'-'+x[4:6]+'-'+x[:4] for x in dates_str]
+             #   date_base_units = []
+             #   date_base_units = [dates_str[i] + ' / ' + base_units[i] for i in range(len(dates_str))]
+             #   date_base_units.sort()
+             #   date_base_unit = st.selectbox('Select date + base unit', date_base_units)
                                 
-                date, base_unit = date_base_unit.split(' / ')
-                day = datetime.datetime.strptime(date, '%d-%m-%Y').date()
+             #   date, base_unit = date_base_unit.split(' / ')
+             #   day = datetime.datetime.strptime(date, '%d-%m-%Y').date()
                 
-                frame['time_text'] = time_text
-                frame_filtered = frame[(frame["Base unit [-]"]==base_unit) & (frame["date [YYYYMMDD]"] == day)]
+             #   frame['time_text'] = time_text
+             #   frame_filtered = frame[(frame["Base unit [-]"]==base_unit) & (frame["date [YYYYMMDD]"] == day)]
                 
-                with st.expander('Properties'):
-                    col1, col2, col3 = st.columns(3)
-                    delta = col1.number_input('Delay time [minutes]', 
-                                              value=5, 
-                                              help='If no drain is installed in this time, it is counted as delay. Default is set to 5 minutes')
-                    delta = delta * 60
+             #   with st.expander('Properties'):
+             #       col1, col2, col3 = st.columns(3)
+             #       delta = col1.number_input('Delay time [minutes]', 
+             #                                 value=5, 
+             #                                 help='If no drain is installed in this time, it is counted as delay. Default is set to 5 minutes')
+             #       delta = delta * 60
                     
-                    start_of_shift = datetime.time((frame_filtered['time [HHMMSS]'].iloc[0]).hour, 0)
-                    start_time = col2.time_input('Start of shift', value=start_of_shift)
-                    start_time = start_time.strftime("%H%M%S")
-                    start_time = pd.Series([start_time])
+             #       start_of_shift = datetime.time((frame_filtered['time [HHMMSS]'].iloc[0]).hour, 0)
+             #       start_time = col2.time_input('Start of shift', value=start_of_shift)
+             #       start_time = start_time.strftime("%H%M%S")
+             #       start_time = pd.Series([start_time])
                     
-                    if frame_filtered['time [HHMMSS]'].iloc[-1] > datetime.time(23, 45):
-                        end_of_shift = datetime.time(23, 59)
-                    else:
-                        end_of_shift = datetime.time((frame_filtered['time [HHMMSS]'].iloc[-1]).hour + 1, 0)
-                    end_time = col3.time_input('End of shift', value=end_of_shift)
-                    end_time = end_time.strftime("%H%M%S")
-                    end_time = pd.Series([end_time])                
+             #       if frame_filtered['time [HHMMSS]'].iloc[-1] > datetime.time(23, 45):
+             #           end_of_shift = datetime.time(23, 59)
+             #       else:
+             #           end_of_shift = datetime.time((frame_filtered['time [HHMMSS]'].iloc[-1]).hour + 1, 0)
+             #       end_time = col3.time_input('End of shift', value=end_of_shift)
+             #       end_time = end_time.strftime("%H%M%S")
+             #       end_time = pd.Series([end_time])                
                 
-                PVD_funcs.show_delay(frame_filtered, delta, start_time, end_time, date, base_unit)
+             #   PVD_funcs.show_delay(frame_filtered, delta, start_time, end_time, date, base_unit)
                 
-                st.markdown(horizontal_line)
-                st.caption('🏗️ New version under construction: https://cofra-cms-processing-tool.streamlitapp.com/')
+             #   st.markdown(horizontal_line)
+             #   st.caption('🏗️ New version under construction: https://cofra-cms-processing-tool.streamlitapp.com/')
                 
                 #dataFrameSerialization = "legacy"
                 #frame["drain_timedelta"] = pd.Timedelta(0)
