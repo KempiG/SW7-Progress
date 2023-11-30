589abf4f3c8b35f87f4164a2366c8d46aaf0e70e
KempiG <84987806+KempiG@users.noreply.github.com>
2023-11-30 22:42:32 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -189,6 +189,7 @@ def main():
             #st.dataframe(df_vis.index)
             if "date" or "Date" in df_vis.columns:
                 chart_data = pd.DataFrame({"Date": df_vis.iloc[:], "Number of installations": df_vis.index})
+                st.dataframe(chart_data)
                 st.bar_chart(chart_data, x='Date',y='Number of installations')
             
             
