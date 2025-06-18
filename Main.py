787f0157f4f8570b8687f7bb58c02799fcad8050
KempiG <84987806+KempiG@users.noreply.github.com>
2025-06-18 10:34:19 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -180,7 +180,9 @@ def main():
             if "date" in df_vis.columns or "Date" in df_vis.columns:
                 date_column = "Install date" if "date" in df_vis.columns else "Install Date"
                 date = pd.to_datetime(df_vis[date_column],errors='coerce')
-                chart_data = pd.DataFrame({"Date": date, "Number of installations": df_vis.index})
+                installation = df_vis.index
+                chart_data = pd.DataFrame({"Date": date, "Number of installations": installation})
+
 
                 # Drop rows with invalid or missing dates
                 chart_data = chart_data.dropna(subset=["Date"])
